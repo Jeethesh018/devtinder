35cc627b3d57c9d7c8a31d83f1ff510144b91184
jeetesh <jeetesh@intense.in>
2025-03-22 10:04:19 +0530

    updated

--- src/server.js
+++ src/server.js
@@ -2,13 +2,27 @@ const express = require("express")
 
 const server = express();
 
-server.use("/document",(req,res)=>{
-    res.send("Welcome to express document")
-})
+// server.use("/document",(req,res)=>{
+//     res.send("Welcome to express document")
+// })
 
 
-server.use("/",(req,res)=>{
-    res.send("Welcome to express js")
+// server.use("/",(req,res)=>{
+//     res.send("Welcome to express js")
+// })
+
+
+server.post("/user/save/:userID", (req, res) => {
+    console.log("Route Params:", req.params); // Correct way to access userID
+    console.log("Query Params:", req.query);  // If query parameters are passed
+    console.log("Request Body:", req.body);   // If body is sent with the request
+    res.send("Added to DB successfully");
+});
+server.get("/user",(req,res)=>{
+    res.send({
+        userid:"1",
+        name:"ABHI"
+    })
 })
 
 
