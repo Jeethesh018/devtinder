913b6fe3ed9bba774e66a676fc7b639d0045a07d
jeetesh <jeetesh@intense.in>
2025-03-23 21:11:23 +0530

    23+03_2025

--- src/server.js
+++ src/server.js
@@ -12,18 +12,40 @@ const server = express();
 // })
 
 
-server.post("/user/save/:userID", (req, res) => {
-    console.log("Route Params:", req.params); // Correct way to access userID
-    console.log("Query Params:", req.query);  // If query parameters are passed
-    console.log("Request Body:", req.body);   // If body is sent with the request
-    res.send("Added to DB successfully");
-});
-server.get("/user",(req,res)=>{
-    res.send({
-        userid:"1",
-        name:"ABHI"
-    })
-})
+// server.post("/user/save/:userID", (req, res) => {
+//     console.log("Route Params:", req.params); // Correct way to access userID
+//     console.log("Query Params:", req.query);  // If query parameters are passed
+//     console.log("Request Body:", req.body);   // If body is sent with the request
+//     res.send("Added to DB successfully");
+// });
+// server.get("/user",(req,res)=>{
+//     res.send({
+//         userid:"1",
+//         name:"ABHI"
+//     })
+// })
+
+
+
+server.get("/user",[(req,res,next)=>{
+   
+    // res.send("response 1 sent successfully")
+    next();
+},
+(req,res,next)=>{
+   
+    // res.send("response 2 sent successfully")
+    next();
+}],[(req,res,next)=>{
+   
+    // res.send("response 1 sent successfully")
+    next();
+},
+(req,res,next)=>{
+   
+    res.send("response 5 sent successfully")
+    next();
+}])
 
 
 
