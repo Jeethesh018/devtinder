9b1a4a0f4f8d1abd3af3638672a83e1d2b5aee2c
jeetesh <jeetesh@intense.in>
2025-03-24 10:59:22 +0530

    day 3 code

--- src/server.js
+++ src/server.js
@@ -2,51 +2,27 @@ const express = require("express")
 
 const server = express();
 
-// server.use("/document",(req,res)=>{
-//     res.send("Welcome to express document")
-// })
-
-
-// server.use("/",(req,res)=>{
-//     res.send("Welcome to express js")
-// })
-
-
-// server.post("/user/save/:userID", (req, res) => {
-//     console.log("Route Params:", req.params); // Correct way to access userID
-//     console.log("Query Params:", req.query);  // If query parameters are passed
-//     console.log("Request Body:", req.body);   // If body is sent with the request
-//     res.send("Added to DB successfully");
-// });
-// server.get("/user",(req,res)=>{
-//     res.send({
-//         userid:"1",
-//         name:"ABHI"
-//     })
-// })
+const {auth} = require("../middleware/auth")
 
 
 
-server.get("/user",[(req,res,next)=>{
+
+server.use("/user",auth,(req,res)=>{
+    // throw new error("hello")
+    res.send("details fetch successfully")
+})
+
+
+server.use("/",auth,(err,req,res,next)=>{
+    console.log(err)
+    if(err){
+        res.status(500).send("something went wrong")
+    }
+    else{
+        next();
+    }
    
-    // res.send("response 1 sent successfully")
-    next();
-},
-(req,res,next)=>{
-   
-    // res.send("response 2 sent successfully")
-    next();
-}],[(req,res,next)=>{
-   
-    // res.send("response 1 sent successfully")
-    next();
-},
-(req,res,next)=>{
-   
-    res.send("response 5 sent successfully")
-    next();
-}])
-
+})
 
 
 server.listen("8000",()=>{
